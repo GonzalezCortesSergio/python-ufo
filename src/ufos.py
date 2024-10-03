3a3328083646b484af9a8cbba98ebc63c8f535b2
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2024-10-03 14:28:26 +0200

    A punto de la desesperacion

--- src/ufos.py
+++ src/ufos.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from math import sqrt
 from collections import namedtuple
 
-
 Avistaminento = namedtuple("Avistamiento", 
                            "fechahora, ciudad, estado, forma, duracion, comentarios, latitud, longitud")
 
@@ -31,3 +30,61 @@ def lee_avistamientos(fichero):
             res.append(tupla)
     
     return res
+
+
+def duracion_total(registros: list, estado: str):
+
+    duracion = 0
+
+    for registro in registros:
+
+        if(registro.estado.__eq__(estado)):
+            duracion+=registro.duracion
+
+    return duracion
+
+
+def comentario_mas_largo(registros: list, anyo: int, palabra: str):
+
+    registros_anyo_palabra = []
+    for registro in registros:
+
+        if(registro.fechahora.year == anyo and registro.comentarios.__contains__(palabra)):
+
+            registros_anyo_palabra.append(registro)
+    
+    
+    comentario = ""
+
+    for registro in registros_anyo_palabra:
+
+        if len(registro.comentarios) > len(comentario):
+            comentario = registro.comentarios
+
+    for registro in registros_anyo_palabra:
+
+        if(registro.comentarios.__eq__(comentario)):
+
+            return registro
+
+def indexa_formas_por_mes(registros):
+
+    meses = {"Enero": set(), "Febrero": set(), "Marzo": set(), "Abril": set(), "Mayo": set(), 
+             "Junio": set(), "Julio": set(), "Agosto": set(), "Septiembre": set(), "Octubre": set(),
+             "Noviembre": set(), "Diciembre": set()}
+    
+    meses_indice= ["Enero", "Febrero", "Marzo", 
+                   "Abril", "Mayo", "Junio", "Julio",
+                   "Agosto", "Septiembre", "Octubre", "Noviembre",
+                   "Diciembre"]
+    
+    
+    for registro in registros:
+
+        if meses.items()[0].__eq__(meses_indice[registro.fechahora.month - 1]):
+
+            meses[meses_indice[registro.fechahora.month - 1]].add(registro.forma)
+
+
+
+    return meses
--- src/ufos_test.py
+++ src/ufos_test.py
@@ -9,15 +9,18 @@ for index, avistamiento in enumerate(lee_avistamientos("./data/ovnis.csv")):
         break
 
 #Test de la función duracion_total
-
-    
+avistamientos = lee_avistamientos("./data/ovnis.csv")
+print(f"Duración total de los avistamientos en in: {duracion_total(avistamientos, "in")}")
+print(f"Duración total de los avistamientos en nm: {duracion_total(avistamientos, "nm")}")
+print(f"Duración total de los avistamientos en pa: {duracion_total(avistamientos, "pa")}")
+print(f"Duración total de los avistamientos en wa: {duracion_total(avistamientos, "wa")}")
 # Test de la función comentario_mas_largo
 
-
+print(comentario_mas_largo(avistamientos, 2005, "ufo"))
 
 # Test de la función avistamientos_fechas
 
-
+print(indexa_formas_por_mes(avistamientos))
 
 # Test de la función hora_mas_avistamientos
 
