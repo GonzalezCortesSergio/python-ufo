4493b5650dafaba88127f3fe482d6a4755f1d0a2
Sergio González Cortés <gonzalez.coser23@triana.salesianos.edu>
2024-10-05 15:10:10 +0200

    Estructura de ejercicios puesta

--- src/ufos_test.py
+++ src/ufos_test.py
@@ -2,6 +2,8 @@ from ufos import *
 
 # Test de la función lee_avistamientos
 
+print("Ejercicio 1: ")
+
 for index, avistamiento in enumerate(lee_avistamientos("./data/ovnis.csv")):
     print(avistamiento)
 
@@ -12,6 +14,9 @@ print("")
 print("")
 
 #Test de la función duracion_total
+
+print("Ejercicio 2: ")
+
 avistamientos = lee_avistamientos("./data/ovnis.csv")
 print(f"Duración total de los avistamientos en in: {duracion_total(avistamientos, "in")}")
 print(f"Duración total de los avistamientos en nm: {duracion_total(avistamientos, "nm")}")
@@ -22,6 +27,8 @@ print("")
 print("")
 # Test de la función comentario_mas_largo
 
+print("Ejercicio 3: ")
+
 print(comentario_mas_largo(avistamientos, 2005, "ufo"))
 
 print("")
@@ -29,12 +36,16 @@ print("")
 
 # Test de la función indexa_formas_por_mes
 
+print("Ejercicio 4: ")
+
 print(indexa_formas_por_mes(avistamientos))
 print("")
 print("")
 
 
 # Test de la función avistamientos_fechas
+
+print("Ejercicio 5: ")
 
 print("Mostrando los avistamientos entre el 1 de mayo de 2005 y el 1 de junio de 2005: \n")
 
@@ -46,6 +57,7 @@ print("")
 
 # Test de la función hora_mas_avistamientos
 
+print("Ejercicio 6: ")
 
 print(f"Hora en la que se han observado más avistamientos: {hora_mas_avistamientos(avistamientos)}")
 
@@ -53,6 +65,8 @@ print("")
 print("")
 
 # Test de la función dicc_estado_longitud_media_comentario
+
+print("Ejercicio 7: ")
 
 print(dicc_estado_longitud_media_comentario(avistamientos))
 
